9da67a872cc5df009c798d84e499ceb7b6a5624d
痕迹 <15035291867@163.com>
2021-10-30 21:08:14 +0800

    feat: 登录注册页面完善

--- cashbook-client/src/pages/Login/index.tsx
+++ cashbook-client/src/pages/Login/index.tsx
@@ -6,6 +6,8 @@ import styles from './index.module.less'
 const {Item} = Form
 const Login = () => {
   const [form] = Form.useForm()
+  const [captchaTmp, setCaptchaTmp] = useState('')
+  const [loading, setLoading] = useState(false)
   const [state, setState] = useState({
     mode: 'login',
     acount: '', // 账号
@@ -23,10 +25,7 @@ const Login = () => {
 
   // 验证码刷新
   const handleChangeCaptcha = (captcha?: string) => {
-    setState({
-      ...state,
-      captcha: captcha as string
-    })
+    setCaptchaTmp(captcha as string)
   }
 
   // 提交
@@ -51,45 +50,58 @@ const Login = () => {
         position: 'top',
       })
     }
+    if (values.captcha && values.captcha !== captchaTmp) {
+      return Toast.show({
+        content: '验证码错误',
+        position: 'top',
+      })
+    }
+    setLoading(true)
+
+  }
+
+  // formItems
+  const renderFormItems = () => {
+    return <div className={styles.box}>
+    <Item name="acount" label="账号" rules={[{ required: true, message: ' ' }]}>
+      <Input
+        placeholder="请输入账号"
+        clearable
+      ></Input>
+    </Item>
+    <Item name="password" label="密码" rules={[{ required: true, message: ' ' }]}>
+      <Input
+        placeholder="请输入密码"
+        clearable
+        type='password'
+      ></Input>
+    </Item>
+    <Item className={styles.captcha} name="captcha" label="验证码" rules={[{ required: true, message: ' ' }]}>
+      <div className={styles.form_item}>
+        <Input
+          placeholder="请输入验证码"
+          clearable
+        ></Input>
+        <Captcha charNum={4} className={styles.captcha} onChange={handleChangeCaptcha}></Captcha>
+      </div>
+    </Item>
+  </div>
   }
 
   return (
     <div className={styles.login_page}>
       <Form className={styles.form_box} form={form} onClick={onFinish} layout='horizontal'
           footer={
-            <Button block color="primary" type="submit">
+            <Button block color="primary" type="submit" loading={loading}>
               {state.mode === 'login' ? "登录" : "注册"}
             </Button>
           }>
         <Tabs activeKey={state.mode} onChange={onChangeMode}>
           <Tabs.TabPane title="登录" key="login">
-            <div className={styles.box}>
-              <Item name="acount" label="账号" rules={[{ required: true, message: ' ' }]}>
-                <Input
-                  placeholder="请输入账号"
-                  clearable
-                ></Input>
-              </Item>
-              <Item name="password" label="密码" rules={[{ required: true, message: ' ' }]}>
-                <Input
-                  placeholder="请输入密码"
-                  clearable
-                  type='password'
-                ></Input>
-              </Item>
-              <Item className={styles.captcha} name="captcha" label="验证码" rules={[{ required: true, message: ' ' }]}>
-                <div className={styles.form_item}>
-                  <Input
-                    placeholder="请输入验证码"
-                    clearable
-                  ></Input>
-                  <Captcha charNum={4} className={styles.captcha} onChange={handleChangeCaptcha}></Captcha>
-                </div>
-              </Item>
-            </div>
+            {renderFormItems()}
           </Tabs.TabPane>
           <Tabs.TabPane title="注册" key="register">
-            <div className="content">可是可是可是上课</div>
+            {renderFormItems()}
           </Tabs.TabPane>
         </Tabs>
       </Form>
